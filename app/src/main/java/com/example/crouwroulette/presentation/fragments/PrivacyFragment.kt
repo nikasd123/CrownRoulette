f3f5bbf61d48dd5bfde8f49b24a651f1b8f7d54a
nikit <nikita_eremeev.95@mail.ru>
2023-09-22 10:40:44 +0300

    PrivacyFragment UI

---
+++ app/src/main/java/com/example/crouwroulette/presentation/fragments/PrivacyFragment.kt
@@ -0,0 +1,18 @@
+package com.example.crouwroulette.presentation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.crouwroulette.R
+
+class PrivacyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_privacy, container, false)
+    }
+}
