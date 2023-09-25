4d86db5a94c161c06548e8fda819bb4eeb2518d6
nikit <nikita_eremeev.95@mail.ru>
2023-09-25 03:21:04 +0300

    Main menu animations

--- app/src/main/java/com/example/crouwroulette/presentation/fragments/GameChoiceFragment.kt
+++ app/src/main/java/com/example/crouwroulette/presentation/fragments/GameChoiceFragment.kt
@@ -1,10 +1,9 @@
 package com.example.crouwroulette.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.crouwroulette.R
 import com.example.crouwroulette.databinding.FragmentGameChoiceBinding
@@ -17,13 +16,36 @@ class GameChoiceFragment : Fragment(R.layout.fragment_game_choice) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentGameChoiceBinding.bind(view)
-        initNavigateFun()
+        initFun()
     }
 
-    private fun initNavigateFun(){
+    private fun initFun() {
         navigateToPrivacyFragment()
         navigateToSlotsGame()
         navigateToRouletteGame()
+
+        animateSlotsGame()
+        animateRouletteGame()
+    }
+
+    private fun animateSlotsGame() {
+        val animations = arrayOf(
+            AnimationUtils.loadAnimation(activity, R.anim.fade_in_fast),
+            AnimationUtils.loadAnimation(activity, R.anim.fade_in_medium),
+            AnimationUtils.loadAnimation(activity, R.anim.fade_in_slow)
+        )
+
+        for (i in 0 until binding.slots.childCount) {
+            val slotIcon = binding.slots.getChildAt(i)
+            val fadeInAnimation = animations[i % animations.size]
+            slotIcon.startAnimation(fadeInAnimation)
+        }
+    }
+
+    private fun animateRouletteGame() {
+        val animation = AnimationUtils.loadAnimation(requireContext(), R.anim.infinit_rotate)
+
+        binding.rouletteSectors.startAnimation(animation)
     }
 
     private fun navigateToPrivacyFragment() {
